f0fd6c7fac93929ce216574014b02e7b80d575f2
Scott Bergler <commandinghands@gmail.com>
2019-02-12 10:11:55 -0800

    remove extraneous spcaing.

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 function Ticket(props){
   return (
     <div>
-        <style jsx>{`
-          div {
-            background-color: red;
-          }
-        `}</style>
+      <style jsx>{`
+        div {
+          background-color: red;
+        }
+      `}</style>
       <h3>{props.location} - {props.names}</h3>
       <p><em>{props.issue}</em></p>
       <hr/>
